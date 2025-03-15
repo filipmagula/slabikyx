f98ba5cf87b50f5b01abc533ccd11bd5cf2042f5
Filip Magula <filip.magula@gmail.com>
2025-03-15 17:18:05 +0100

    gh6

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,9 @@ import ReadingDisplay from './components/ReadingDisplay.vue';
 
 <template>
   <div class="min-h-screen bg-gray-50 py-8">
-    <div class="container mx-auto">
+    <div class="container mx-auto px-4">
       <h1 class="text-4xl font-bold text-center text-blue-600 mb-8">
-        Aplikace pro učení čtení
+        Čtení a slabiky
       </h1>
       
       <TextInput />
